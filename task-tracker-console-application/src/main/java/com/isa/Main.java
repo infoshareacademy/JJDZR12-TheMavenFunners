a418b0e2bfbcb664db8ac70cbf9f32947615aa7e
Jakub Czarnota <kuba.czarnota04@gmail.com>
2023-08-20 13:50:01 +0200

    Main Menu added

---
+++ task-tracker-console-application/src/main/java/com/isa/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import com.isa.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+    MainMenu.displayMainMenu();
+    MainMenu.printUserMainMenuChoice();
+    }
+}

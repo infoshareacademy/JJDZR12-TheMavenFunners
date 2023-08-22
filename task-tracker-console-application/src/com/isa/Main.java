ac0b029b11a22d3b09bf59cf0ca37a5c0f6e9b50
Jakub Czarnota <kuba.czarnota04@gmail.com>
2023-08-22 18:51:23 +0200

    Package organization

---
+++ task-tracker-console-application/src/com/isa/Main.java
@@ -0,0 +1,10 @@
+package com.isa;
+
+import com.isa.MainMenu.MainMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenu.displayMainMenu();
+        MainMenu.printUserMainMenuChoice();
+    }
+}

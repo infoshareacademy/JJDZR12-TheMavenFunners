d68097c2391eaf92059a6af2d5d4af2f8fe16970
Dominika Gawron <dmgawron7@gmail.com>
2023-08-28 19:52:16 +0200

    added user login

---
+++ task-tracker-console-application/src/com/isa/user/Login.java
@@ -0,0 +1,134 @@
+package com.isa.user;
+
+import com.isa.MainMenu.MainMenu;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Login {
+    private static User loggedInUser;
+    private static final List<User> users = User.getTestUsers();
+    private static String inputLogin;
+
+    public static User getLoggedInUser() {
+        return loggedInUser;
+    }
+
+    public static void logOutUser() {
+        loggedInUser = null;
+    }
+
+    private static void displayLoginMessage() {
+        System.out.println("=====================================");
+        System.out.println("             ZALOGUJ SIĘ");
+        System.out.println("=====================================");
+        System.out.println("Podaj dane logowania lub wybierz \"0\", żeby powrócić do menu głównego.");
+    }
+
+    private static void logInUser(Scanner scanner) {
+        System.out.print("Podaj login: ");
+        validateLogin(scanner, 0);
+    }
+
+    private static void validateLogin(Scanner scanner, int incorrectLoginInputCount) {
+        inputLogin = scanner.nextLine();
+        if (inputLogin.equals("0")) {
+            goToMainMenu();
+        } else if (chosenUser() == null) {
+            incorrectLoginInputCount++;
+            System.out.println("Podany login nie istnieje.");
+            if (incorrectLoginInputCount > 1) {
+                System.out.println("Pamiętaj, że w każdej chwili możesz wrócić do menu głównego poprzez wybór \"0\".");
+            }
+
+            System.out.print("Podaj login ponownie: ");
+            validateLogin(scanner, incorrectLoginInputCount);
+        } else {
+            System.out.print("Podaj hasło: ");
+            validatePassword(scanner, 0);
+        }
+    }
+
+    private static User chosenUser() {
+        for (User u : users) {
+            if (u.getLogin().equals(inputLogin)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    private static void validatePassword(Scanner scanner, int incorrectPasswordInputCount) {
+        String inputPassword = scanner.nextLine();
+        if (inputPassword.equals("0")) {
+            goToMainMenu();
+        } else if (!isCorrectPassword(inputPassword)) {
+            incorrectPasswordInputCount++;
+            System.out.println("Hasło niepoprawne.");
+            if (incorrectPasswordInputCount > 1 && incorrectPasswordInputCount < 5) {
+                System.out.println("Pamiętaj, że w każdej chwili możesz wrócić do menu głównego poprzez wybór \"0\".");
+                if (incorrectPasswordInputCount == 4) {
+                    System.out.println("UWAGA: Przy kolejnej nieprawidłowej próbie zalogowania się twoje konto zostanie zablokowane.");
+                }
+            }
+            if (incorrectPasswordInputCount != 5) {
+                System.out.print("Podaj hasło ponownie: ");
+                validatePassword(scanner, incorrectPasswordInputCount);
+            } else {
+                chosenUser().setActive(false);
+                System.out.println("Wykryto 5 nieprawidłowych prób zalogowania się. Twoje konto zostało zablokowane.");
+                askForBlockedUserChoice(scanner);
+            }
+        } else {
+            loggedInUser = chosenUser();
+            printWelcomeMessage();
+            if (loggedInUser.isActive()) {
+                /* TODO Replace the message below once the menu for the logged in user is created */
+                System.out.println("Menu po zalogowaniu się niedostępne. Nastąpi samoczynne zakończenie programu.");
+            } else {
+                askForBlockedUserChoice(scanner);
+            }
+        }
+    }
+
+    private static boolean isCorrectPassword(String inputPassword) {
+        return inputPassword.equals(chosenUser().getPassword());
+    }
+
+    private static void printWelcomeMessage() {
+        if (loggedInUser.isActive()) {
+            System.out.print("Zalogowano poprawnie. Witaj, ");
+            System.out.print(loggedInUser.getFirstName() != null && !loggedInUser.getFirstName().isBlank() ? loggedInUser.getFirstName() : loggedInUser.getLogin());
+            System.out.println("!");
+        } else {
+            System.out.println("Twoje konto jest zablokowane. Jeśli potrzebujesz dostępu, skontaktuj się z administratorem.");
+        }
+    }
+
+    private static void askForBlockedUserChoice(Scanner scanner) {
+        System.out.println("Wybierz \"0\", żeby wrócić do menu głównegu, lub \"x\", żeby zakończyć program.");
+        String blockedUserInput = scanner.nextLine();
+        if (blockedUserInput.equals("0")) {
+            logOutUser();
+            goToMainMenu();
+        } else if (blockedUserInput.equals("x")) {
+            logOutUser();
+            System.exit(0);
+        } else {
+            System.out.println("Wprowadź prawidłową wartość.");
+            askForBlockedUserChoice(scanner);
+        }
+    }
+
+    private static void goToMainMenu() {
+        MainMenu.displayMainMenu();
+        MainMenu.printUserMainMenuChoice();
+    }
+
+    public static void start() {
+        Scanner scanner = new Scanner(System.in);
+        displayLoginMessage();
+        logInUser(scanner);
+        scanner.close();
+    }
+}

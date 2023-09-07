850dbd7496c8ad34dd7c25c31c7fb0e40de05262
Piotr Kaczmarek <piotr.kaczmarek101@gmail.com>
2023-09-07 07:58:26 +0200

    Task file added

---
+++ task-tracker-console-application/src/main/java/com/isa/task/Task.java
@@ -0,0 +1,12 @@
+package com.isa.task;
+
+import com.isa.user.User;
+
+public class Task {
+    private String taskName;
+    private String taskStart;
+    private String taskEnd;
+    private String taskDescription;
+    private User login;
+    private Boolean active;
+}

40e20b515892d04e60a804494bc9b72859167251
Piotr Kaczmarek <piotr.kaczmarek101@gmail.com>
2023-09-07 08:06:50 +0200

    file task.java modified

--- task-tracker-console-application/src/main/java/com/isa/task/Task.java
+++ task-tracker-console-application/src/main/java/com/isa/task/Task.java
@@ -9,4 +9,61 @@ public class Task {
     private String taskDescription;
     private User login;
     private Boolean active;
+
+    public Task(String taskName, String taskStart, String taskEnd, String taskDescription, Boolean active) {
+        this.taskName = taskName;
+        this.taskStart = taskStart;
+        this.taskEnd = taskEnd;
+        this.taskDescription = taskDescription;
+        this.active = active;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskStart() {
+        return taskStart;
+    }
+
+    public void setTaskStart(String taskStart) {
+        this.taskStart = taskStart;
+    }
+
+    public String getTaskEnd() {
+        return taskEnd;
+    }
+
+    public void setTaskEnd(String taskEnd) {
+        this.taskEnd = taskEnd;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public User getLogin() {
+        return login;
+    }
+
+    public void setLogin(User login) {
+        this.login = login;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
+

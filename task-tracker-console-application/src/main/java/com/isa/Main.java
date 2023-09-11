2371ed9d8deb17159eaa4cb1afd414c62316c956
Dominika Gawron <dmgawron7@gmail.com>
2023-09-12 00:41:09 +0200

    removed unused imports

--- task-tracker-console-application/src/main/java/com/isa/Main.java
+++ task-tracker-console-application/src/main/java/com/isa/Main.java
@@ -1,8 +1,6 @@
 package com.isa;
 
-import com.isa.menu.AdminMenu;
 import com.isa.menu.MainMenu;
-import com.isa.menu.MenuAfterLoggingIn;
 
 public class Main {
     public static void main(String[] args) {

b0f77e3b1b92a13fb98d18a534c3f9dbf37c938e
Dominika Gawron <dmgawron7@gmail.com>
2023-09-11 21:13:04 +0200

    fixed issues according to pr comments

--- task-tracker-console-application/src/main/java/com/isa/data/JsonData.java
+++ task-tracker-console-application/src/main/java/com/isa/data/JsonData.java
@@ -5,16 +5,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.isa.user.User;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class JsonData {
-    private static List<User> users;
+    private static List<User> users = new ArrayList<>();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     private static final File userFile = new File("task-tracker-console-application/src/main/resources/database/users.json");
 
     public static List<User> getUsers() {
-        ObjectMapper objectMapper = new ObjectMapper();
         try {
-            users = objectMapper.readValue(userFile, new TypeReference<List<User>>() {
+            users = objectMapper.readValue(userFile, new TypeReference<>() {
             });
         } catch (Exception e) {
             e.printStackTrace();
@@ -23,8 +24,7 @@ public class JsonData {
     }
 
     public static void saveNewUser(User user) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        users = getUsers();
+        users.addAll(getUsers());
         users.add(user);
         try {
             objectMapper.writeValue(userFile, users);

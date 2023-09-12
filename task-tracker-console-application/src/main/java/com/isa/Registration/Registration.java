c50378a00a1569603ad2634763c540080174f9d1
dominikstyfi <styfi@post.pl>
2023-09-12 22:55:44 +0200

    added error handling

--- task-tracker-console-application/src/main/java/com/isa/Registration/Registration.java
+++ task-tracker-console-application/src/main/java/com/isa/Registration/Registration.java
@@ -1,21 +1,29 @@
 package com.isa.Registration;
+
 import com.isa.user.User;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Registration {
-        private List<User> users;
+    private List<User> users;
+    private Object args;
 
-        public Registration() {
-            users = new ArrayList<>();
-        }
+    public Registration() {
+        users = new ArrayList<>();
+    }
 
-        public boolean isUserExists(String login) {
-            return users.stream()
-                    .anyMatch(user -> user.getLogin().equals(login));
-        }
+    public boolean isUserExists(String login) {
+        return users.stream()
+                .anyMatch(user -> user.getLogin().equals(login));
+    }
 
-        public void registerUser(String firstName, String lastName, String login, String password) {
+    public void registerUser(String firstName, String lastName, String login, String password) {
+        try {
+            if (firstName.isEmpty() || lastName.isEmpty() || login.isEmpty() || password.isEmpty()) {
+                throw new IllegalArgumentException("Wszystkie pola są wymagane.");
+            }
+
             if (isUserExists(login)) {
                 System.out.println("Użytkownik istnieje.");
             } else {
@@ -26,7 +34,8 @@ public class Registration {
                 System.out.println("Konto zostało utworzone.");
                 System.out.println("Automatycznie przejście do opcji logowania...");
             }
-        }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Błąd: " + e.getMessage());
 
         public static void main(String[] args) {
             Registration registration = new Registration();

3b04061201d286dab78ff9a3fb80b9b3d95c80bf
Jakub Czarnota <kuba.czarnota04@gmail.com>
2023-10-08 09:07:04 +0200

    Fixes

--- task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/TaskTrackerWebappApplication.java
+++ task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/TaskTrackerWebappApplication.java
@@ -1,4 +1,4 @@
-package com.example.tasktrackerwebapp;
+package com.isa.tasktrackerwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

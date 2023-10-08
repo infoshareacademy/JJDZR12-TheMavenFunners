35d6015d05345bd9c08cf12ef7e335c74a4dd909
Jakub Czarnota <kuba.czarnota04@gmail.com>
2023-10-08 09:01:08 +0200

    Created new module

---
+++ task-tracker-webapp/src/main/java/com/example/tasktrackerwebapp/TaskTrackerWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasktrackerwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerWebappApplication.class, args);
+    }
+
+}

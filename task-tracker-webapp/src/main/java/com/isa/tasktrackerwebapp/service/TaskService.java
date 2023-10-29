067aa72b25751aaf3192b67ad35654fe8f437d65
Jakub Czarnota <kuba.czarnota04@gmail.com>
2023-10-29 11:30:32 +0100

    fixes

--- task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/service/TaskService.java
+++ task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/service/TaskService.java
@@ -10,30 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public interface TaskService {
-    public static List<Task> createSampleTasks() {
-        List<Task> taskList = new ArrayList<>();
-
-        User user1 = new User("user1", "password1", "user1@example.com", true);
-        User user2 = new User("user2", "password2", "user2@example.com", true);
-        LocalDate now = LocalDate.now();
-        Task task1 = new Task("Task 1", now.minusDays(3), now.plusDays(1), "Description 1", user1, false);
-        Task task2 = new Task("Task 2", now.minusDays(2), now.plusDays(2), "Description 2", user1, false);
-        Task task3 = new Task("Task 3", now.minusDays(1), now.plusDays(3), "Description 3", user2, true);
-        Task task4 = new Task("Task 4", now.minusDays(4), now.plusDays(5), "Description 4", user2, true);
-        Task task5 = new Task("Task 5", now.minusDays(5), now.plusDays(4), "Description 5", user1, false);
-        Task task6 = new Task("Task 1", now.minusDays(2), now.plusDays(1), "Description 1", user1, true);
-        Task task7 = new Task("Task 1", now.minusDays(1), now.plusDays(1), "Description 1", user1, false);
-
-        taskList.add(task1);
-        taskList.add(task2);
-        taskList.add(task3);
-        taskList.add(task4);
-        taskList.add(task5);
-        taskList.add(task6);
-        taskList.add(task7);
-        return taskList;
-    }
-
+    
     private static List<Task> sortTaskByStartDateDes(List<Task> taskList) {
         taskList.sort((task1, task2) -> task2.getTaskStart().compareTo(task1.getTaskStart()));
         return taskList;

5c92d752f97202db880dbc55a89c607823c941b0
Maciej Malik <malik-malik@o2.pl>
2023-11-05 12:17:06 +0100

    Fix service

--- task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/controller/TaskController.java
+++ task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/controller/TaskController.java
@@ -1,6 +1,5 @@
 package com.isa.tasktrackerwebapp.controller;
 
-import com.isa.tasktrackerwebapp.model.JsonDataTask;
 import com.isa.tasktrackerwebapp.model.Task;
 import com.isa.tasktrackerwebapp.service.TaskService;
 import org.springframework.stereotype.Controller;
@@ -11,22 +10,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-public class TaskController {
+class TaskController {
+
     private final TaskService taskService;
 
-    public TaskController(TaskService taskService) {
+    TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
 
     @GetMapping("/taskList")
-    public String taskList(Model model, @RequestParam(required = false) String sortBy, @RequestParam(required = false) String searchTaskName, @RequestParam(required = false) String filterActive) {
-        List<Task> taskList = taskService.getTasks();
-
-        taskList = taskService.sortAndFilterTasks(sortBy, searchTaskName, filterActive, taskList);
-
+    String taskList(Model model,
+                    @RequestParam(required = false) String sortBy,
+                    @RequestParam(required = false) String searchTaskName,
+                    @RequestParam(required = false) String filterActive) {
+        List<Task> taskList = taskService.getSortedAndFilteredTasks(sortBy, searchTaskName, filterActive);
         model.addAttribute("taskList", taskList);
         return "taskList";
     }
-
-
 }
--- task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/service/TaskService.java
+++ task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/service/TaskService.java
@@ -1,65 +1,10 @@
 package com.isa.tasktrackerwebapp.service;
 
-import com.isa.tasktrackerwebapp.model.JsonDataTask;
 import com.isa.tasktrackerwebapp.model.Task;
-import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Service
-public class TaskService {
+public interface TaskService {
 
-
-    public static List<Task> filterTasks(List<Task> taskList, String searchTaskName, String filterActive) {
-        return taskList.stream()
-                .filter(task -> {
-                    boolean nameMatch = searchTaskName == null || searchTaskName.isEmpty() || task.getTaskName().toLowerCase().contains(searchTaskName.toLowerCase());
-                    boolean activeMatch = filterActive == null || filterActive.isEmpty() || task.getActive() == Boolean.parseBoolean(filterActive);
-                    return nameMatch && activeMatch;
-                })
-                .collect(Collectors.toList());
-    }
-
-    public List<Task> sortAndFilterTasks(String sortBy, String searchTaskName, String filterActive, List<Task> taskList) {
-        if ("oldestByStartDate".equals(sortBy)) {
-            taskList.sort(Comparator.comparing(Task::getTaskStart));
-        } else if ("newestByStartDate".equals(sortBy)) {
-            taskList.sort(Comparator.comparing(Task::getTaskStart).reversed());
-        } else if ("oldestByEndDate".equals(sortBy)) {
-            taskList.sort(Comparator.comparing(Task::getTaskEnd));
-        } else if ("newestByEndDate".equals(sortBy)) {
-            taskList.sort(Comparator.comparing(Task::getTaskEnd).reversed());
-        }
-
-        taskList = filterTasks(taskList, searchTaskName, filterActive);
-
-        return taskList;
-    }
-
-    public List<Task> getTasks() {
-        return JsonDataTask.getTasks();
-    }
-
-    private List<Task> sortTaskByStartDateDes(List<Task> taskList) {
-        taskList.sort((task1, task2) -> task2.getTaskStart().compareTo(task1.getTaskStart()));
-        return taskList;
-    }
-
-    private List<Task> sortTaskByStartDateAsc(List<Task> taskList) {
-        Collections.reverse(sortTaskByStartDateDes(taskList));
-        return taskList;
-    }
-
-    private List<Task> sortTaskByEndDateDes(List<Task> taskList) {
-        taskList.sort((task1, task2) -> task2.getTaskEnd().compareTo(task1.getTaskEnd()));
-        return taskList;
-    }
-
-    private List<Task> sortTaskByEndDateAsc(List<Task> taskList) {
-        Collections.reverse(sortTaskByEndDateDes(taskList));
-        return taskList;
-    }
+    List<Task> getSortedAndFilteredTasks(String sortBy, String searchTaskName, String filterActive);
 }

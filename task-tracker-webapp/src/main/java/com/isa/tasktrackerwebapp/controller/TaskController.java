79931f0e453063087b965ed5aa277ccbaf0f8b9e
Dominika Gawron <dmgawron7@gmail.com>
2023-11-06 16:33:16 +0100

    modified styles, fixed controllers

--- task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/controller/TaskController.java
+++ task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.isa.tasktrackerwebapp.controller;
 
+import com.isa.tasktrackerwebapp.model.PageType;
 import com.isa.tasktrackerwebapp.model.Task;
 import com.isa.tasktrackerwebapp.service.TaskService;
 import org.springframework.stereotype.Controller;
@@ -18,13 +19,15 @@ class TaskController {
         this.taskService = taskService;
     }
 
-    @GetMapping("/taskList")
+    @GetMapping("/view-tasks")
     String taskList(Model model,
                     @RequestParam(required = false) String sortBy,
                     @RequestParam(required = false) String searchTaskName,
                     @RequestParam(required = false) String filterActive) {
         List<Task> taskList = taskService.getSortedAndFilteredTasks(sortBy, searchTaskName, filterActive);
-        model.addAttribute("taskList", taskList);
-        return "taskList";
+        model.addAttribute("taskList", taskList)
+                .addAttribute("content", PageType.VIEW_TASKS.getContentValue())
+                .addAttribute("pageTitle", PageType.VIEW_TASKS.getTitleValue());
+        return "main";
     }
 }

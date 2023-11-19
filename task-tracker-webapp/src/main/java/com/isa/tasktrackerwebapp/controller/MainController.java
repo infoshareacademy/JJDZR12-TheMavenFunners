51ae83d3b3d8637d8eae53aa979b571b28fdd0db
Dominika Gawron <dmgawron7@gmail.com>
2023-11-19 01:07:03 +0100

    added css styles to add-task

--- task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/controller/MainController.java
+++ task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/controller/MainController.java
@@ -1,13 +1,23 @@
 package com.isa.tasktrackerwebapp.controller;
 
 import com.isa.tasktrackerwebapp.model.PageType;
+import com.isa.tasktrackerwebapp.model.Task;
+import com.isa.tasktrackerwebapp.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class MainController {
+
+    private final TaskService taskService;
+
+    MainController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @GetMapping("/")
     String index(Model model) {
@@ -26,7 +36,14 @@ public class MainController {
     @GetMapping("/add-task")
     String newTask(Model model) {
         model.addAttribute("content", PageType.ADD_TASK.getContentValue())
-                .addAttribute("pageTitle", PageType.ADD_TASK.getTitleValue());
+                .addAttribute("pageTitle", PageType.ADD_TASK.getTitleValue())
+                .addAttribute("task", new Task());
         return "main";
+    }
+
+    @PostMapping("/add-task")
+    String saveTask(@ModelAttribute Task form) {
+        taskService.saveTask(form);
+        return "redirect:add-task";
     }
 }

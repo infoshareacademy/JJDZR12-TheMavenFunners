3657ae6f2cbc8f3b86cb984283a009aeded19792
Maciej Malik <malik-malik@o2.pl>
2023-11-19 13:46:33 +0100

    Refactor

--- task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/controller/MainController.java
+++ task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/controller/MainController.java
@@ -1,23 +1,12 @@
 package com.isa.tasktrackerwebapp.controller;
 
 import com.isa.tasktrackerwebapp.model.PageType;
-import com.isa.tasktrackerwebapp.model.Task;
-import com.isa.tasktrackerwebapp.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
 
 @Controller
 public class MainController {
-
-    private final TaskService taskService;
-
-    MainController(TaskService taskService) {
-        this.taskService = taskService;
-    }
 
     @GetMapping("/")
     String index(Model model) {
@@ -30,27 +19,6 @@ public class MainController {
     String registration(Model model) {
         model.addAttribute("content", PageType.REGISTRATION.getContentValue())
                 .addAttribute("pageTitle", PageType.REGISTRATION.getTitleValue());
-        return "main";
-    }
-
-    @GetMapping("/add-task")
-    String newTask(Model model) {
-        model.addAttribute("content", PageType.ADD_TASK.getContentValue())
-                .addAttribute("pageTitle", PageType.ADD_TASK.getTitleValue())
-                .addAttribute("task", new Task());
-        return "main";
-    }
-
-    @PostMapping("/add-task")
-    String saveTask(@ModelAttribute Task form) {
-        taskService.saveTask(form);
-        return "redirect:add-task";
-    }
-
-    @GetMapping("/view-tasks")
-    String viewTasks(Model model) {
-        model.addAttribute("content", PageType.VIEW_TASKS.getContentValue())
-                .addAttribute("pageTitle", PageType.VIEW_TASKS.getTitleValue());
         return "main";
     }
 }

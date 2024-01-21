daa99f6f90d5b797db887ead0c96fb545e85a5fa
Piotr Kaczmarek <piotr.kaczmarek101@gmail.com>
2024-01-21 11:41:24 +0100

    "Security configuration added"

--- task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/model/entity/User.java
+++ task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/model/entity/User.java
@@ -2,13 +2,16 @@ package com.isa.tasktrackerwebapp.model.entity;
 
 import jakarta.validation.constraints.*;
 
+import java.util.Collection;
 import java.util.Objects;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -102,5 +105,35 @@ public class User {
     @Override
     public String toString() {
         return login;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }

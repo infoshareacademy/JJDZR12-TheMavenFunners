de2421212f98a70cd02fcb7dc47b791cb3b4114d
Maciej Malik <malik-maciej@o2.pl>
2024-01-25 21:50:49 +0100

    Fix security

--- task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/config/SecurityConfig.java
+++ task-tracker-webapp/src/main/java/com/isa/tasktrackerwebapp/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 class SecurityConfig {
+  
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
@@ -23,6 +24,7 @@ class SecurityConfig {
             .anyRequest().authenticated())
         .formLogin(form -> form
             .loginPage("/login")
+            .usernameParameter("login")
             .successForwardUrl("/")
             .failureUrl("/login?error=true"))
         .logout(logout -> logout
